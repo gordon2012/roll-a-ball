9e88cc77eb0efec895bf871ff4ddcb42b0051f6f
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2017-08-03 07:23:24 -0700

    Collecting the Pick Up Objects
    Signed-off-by: Gordon Doskas <gdoskas2012@email.phoenix.edu>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,23 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	private Rigidbody rb;
 
-	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 		
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce(movement * speed);
-		
-		//Input
 	}
+
+	void OnTriggerEnter(Collider other) {
+        // Destroy(other.gameObject);
+
+		if (other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+		}
+    }
 }
